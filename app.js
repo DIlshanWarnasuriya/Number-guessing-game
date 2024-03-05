8a9889d224839abc3085284a82ffa847588696ec
DilshanWarnasuriya <dilshan.se.dew@gmail.com>
2024-03-06 01:29:27 +0530

    Design User Interface

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 let RandomNumber = Math.round(Math.random() * 20);
+console.log(RandomNumber);
 document.getElementById("txtNum").value = "";
 let body;
 let count = 3;
 
-let submit = () => {
+let guess = () => {
     let inputnumber = Number(document.getElementById("txtNum").value);
+    let WarningMessage = document.getElementById("WarningMessage");
 
     if (inputnumber >= 0 && inputnumber <= 20 && inputnumber != "") {
         if (count > 0) {
             if (inputnumber == RandomNumber) {
-                body = `<h3>Congratulations!. You was guess number correctly</h3>`;
-                document.getElementById("result").innerHTML = body;
+                WarningMessage.style.visibility = "visible"
+                WarningMessage.style.backgroundColor = "#27ae60"
+                WarningMessage.innerHTML="Congratulations!. You was guess number correctly";
             }
             else if (inputnumber > RandomNumber) {
-                body = `<h3> Wrong guess! Your Answer is more than Random Number. You have ${--count} more attempt </h3>`;
-                document.getElementById("result").innerHTML = body;
+                WarningMessage.style.visibility = "visible"
+                WarningMessage.style.backgroundColor = "#d35400"
+                WarningMessage.innerHTML="Wrong guess! Your Answer is more than Random Number. You have " + --count + " more attempt ";
             }
             else {
-                body = `<h3> Wrong guess! Your Answer is less than Random Number. You have ${--count} more attempt</h3>`;
-                document.getElementById("result").innerHTML = body;
+                WarningMessage.style.visibility = "visible"
+                WarningMessage.style.backgroundColor = "#d35400"
+                WarningMessage.innerHTML="Wrong guess! Your Answer is less than Random Number. You have " + --count + " more attempt ";
             }
         }
         else {
-            body = `<h3>Your attempts are over</h3>`;
-            document.getElementById("result").innerHTML = body;
+            WarningMessage.style.visibility = "visible"
+            WarningMessage.style.backgroundColor = "#e74c3c"
+            WarningMessage.innerHTML="Your attempts are over";
         }
     }
     else {
-        body = `<h3>Please enter between 0 to 20 number</h3>`;
-        document.getElementById("result").innerHTML = body;
-    }
-
+        WarningMessage.style.visibility = "visible" 
+        WarningMessage.style.backgroundColor = "#e74c3c"
+        WarningMessage.innerHTML="Please enter between 0 to 20 number";
+    }   
 }
 
